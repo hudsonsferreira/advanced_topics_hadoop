8a06c65de330760aebbc0846e9578f678172301d
Hudson <silvaferreira.hsf@gmail.com>
2015-10-29 21:14:39 -0200

    Add WordCount::Mapper and Reducer classes

---
+++ lib/word_count/reducer.rb
@@ -0,0 +1,9 @@
+module WordCount
+  class Reducer
+    def reduce(key, values, context)
+      sum = 0
+      values.each { |value| sum += value.get }
+      context.write(key, Hadoop::Io::IntWritable.new(sum))
+    end
+  end
+end
